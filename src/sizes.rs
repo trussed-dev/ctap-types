db1526266a463e0c596969d545c916847e182e2a
Felix Gohla <felix.gohla@student.hpi.de>
2021-12-26 10:03:37 +0100

    Add authenticatorLargeBlobs Types

--- src/sizes.rs
+++ src/sizes.rs
@@ -24,3 +24,7 @@ pub const THEORETICAL_MAX_MESSAGE_SIZE: usize = PACKET_SIZE - 7 + 128 * (PACKET_
 /// The size used by Yubico, which means that no platforms will
 /// realistically expect a larger size.
 pub const REALISTIC_MAX_MESSAGE_SIZE: usize = 1200;
+
+/// Max length for a large blob fragment, according to
+/// https://fidoalliance.org/specs/fido-v2.1-ps-20210615/fido-client-to-authenticator-protocol-v2.1-ps-20210615.html#largeBlobsRW
+pub const LARGE_BLOB_MAX_FRAGMENT_LENGTH: usize = REALISTIC_MAX_MESSAGE_SIZE - 64;
